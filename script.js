8cf1b53d025dc652e51e136a9b32dc1bdd133c67
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-04-28 08:50:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+#yellow{
+	background-color:Yellow;
+	border-width:1px;
+	border-color:green;
+}
+#red{
+    background-color:red;
+	border-width:1px;	
+	border-color:black;
+}
+#green{
+	background-color:red;
+	border-width:1px;	
+	border-color:black;
+}
